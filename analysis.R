010753b49923004ac28f85b3e7c43d1c02505d5e
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-02-11 22:25:19 +0100

    first draft of simulations

---
+++ analysis.R
@@ -0,0 +1,56 @@
+library(GOSemSim)
+library(ggplot2)
+library(pbapply)
+
+hsGO <- godata('org.Hs.eg.db', ont="MF")
+
+go1  <- c("GO:0004022","GO:0004024","GO:0004174")
+
+go2 <- c("GO:0009055","GO:0005515")
+
+mgoSim(go1, go2, semData = hsGO, measure = "Wang", combine = NULL)
+
+finite_go <- names(slot(hsGO, "IC")[!is.infinite(slot(hsGO, "IC"))])
+
+set.seed(15922)
+
+sims <- do.call(rbind, pblapply(1L:10000, function(dummy) {
+  x1 <- sample(x = finite_go, size = sample(1L:6, 1), replace = FALSE)
+  x2 <- sample(x = finite_go, size = sample(1L:6, 1), replace = FALSE)
+  
+  data.frame(
+    x1 = paste0(x1, collapse = ";"),
+    x2 = paste0(x2, collapse = ";"),
+    sim = mgoSim(x1, x2, semData = hsGO, measure = "Rel", combine = "BMA"))
+}))
+
+png("far.png")
+ggplot(sims, aes(x = sim)) +
+  geom_density() +
+  #geom_vline(aes(xintercept = median(sim)), color = "green") +
+  geom_vline(aes(xintercept = mean(sim)), color = "blue") +
+  scale_x_continuous("Similarity") +
+  theme_bw()
+dev.off()
+
+
+sims_close <- do.call(rbind, pblapply(1L:10000, function(dummy) {
+  x1 <- sample(x = finite_go, size = sample(1L:6, 1), replace = FALSE)
+  x2 <- c(x1, sample(x = setdiff(finite_go, x1), size = sample(1L:3, 1), replace = FALSE))
+  
+  data.frame(
+    x1 = paste0(x1, collapse = ";"),
+    x2 = paste0(x2, collapse = ";"),
+    sim = mgoSim(x1, x2, semData = hsGO, measure = "Rel", combine = "BMA"))
+}))
+
+png("close.png")
+ggplot(sims_close, aes(x = sim)) +
+  geom_density() +
+  #geom_vline(aes(xintercept = median(sim, na.rm = TRUE)), color = "green") +
+  geom_vline(aes(xintercept = mean(sim, na.rm = TRUE)), color = "blue") +
+  scale_x_continuous("Similarity")+
+  theme_bw()
+dev.off()
+
+save(sims, sims_close, file = "sims.RData")
